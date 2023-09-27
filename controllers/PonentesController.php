011fd6d724581acd628f3d8d1962129ec1e906c9
Nahuel Burgueño <nahuelburge@gmail.com>
2023-09-27 20:36:09 -0300

    Cambios en ponentes
    Subir imágenes(webp, png), listado de ponentes, crear ponente, guardar en bd, y editar-eliminar ponentes.

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -3,19 +3,73 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Ponente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
     public static function index(Router $router)  {
+
+        $ponentes = Ponente::all();
+
+
         $router->render('admin/ponentes/index', [
-            'titulo' => 'Ponentes / Conferencistas'
+            'titulo' => 'Ponentes / Conferencistas',
+            'ponentes' => $ponentes
         ]);
     }
-    public static function crear(Router $router)  {
 
+    public static function crear(Router $router)  {
         $alertas = [];
+        $ponente = new Ponente;
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Leer imagen
+            if(!empty($_FILES['imagen']['tmp_name'])) {
+                
+                $carpeta_imagenes = '../public/img/speakers';
+
+                // Crear la carpeta si no existe
+                if(!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes,  0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
+
+                $nombre_imagen = md5( uniqid ( rand(), true ) );
+
+                $_POST['imagen'] = $nombre_imagen;
+            }
+
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES );
+
+            $ponente->sincronizar($_POST);
+
+            // Validar
+            $alertas = $ponente->validar();
+
+            // Guardar registro
+            if(empty($alertas)) {
+
+                // Guardar imagenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+
+                // Guardar en la base de datos
+
+                $resultado = $ponente->guardar();
+
+                if($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
+
         $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponentes',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'ponente' => $ponente
         ]);
     }
 }
--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -3,3 +3,43 @@
 <div class="dashboard__contenedor-boton">
     <a class="dashboard__boton" href="/admin/ponentes/crear"><i class="fa-solid fa-circle-plus"></i>Añadir Ponente</a>
 </div>
+
+<div class="dashboard__contenedor">
+
+<?php if(!empty($ponentes)) {  ?>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col" class="table__th">Nombre</th>
+                <th scope="col" class="table__th">Ubicación</th>
+                <th scope="col" class="table__th"></th>
+            </tr>
+        </thead> 
+
+
+    <tbody class="table__tbody"> 
+    <?php foreach($ponentes as $ponente) { ?>
+        <tr class="table__tr">
+            <td class="table__td"><?php echo $ponente->nombre . " " . $ponente->apellido; ?></td>
+            <td class="table__td"><?php echo $ponente->ciudad . "," . $ponente->pais; ?></td>
+
+            <td class="table__td-acciones">
+                <a href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>"> <i class="fa-solid fa-user-pen"></i> Editar</a>
+
+                <form action="" class="table__formulario">
+                    <button type="submit"><i class="fa-solid fa-circle-xmark"></i> Eliminar</button>
+                </form>
+            </td>
+        </tr>
+
+    <?php  } ?>
+        </tbody>
+    </table>
+<?php } else { ?> 
+
+<p class="text-center">No hay ponentes aún</p>
+
+<?php }  ?> 
+
+</div>
